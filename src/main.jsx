a7dfdfe895c44ebb980e696814594b256835285a
duli1982 <130799479+duli1982@users.noreply.github.com>
2025-08-27 06:29:38 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TalentSonarApp from "./TalentSonarApp";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TalentSonarApp from './TalentSonarApp.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TalentSonarApp />
-  </React.StrictMode>
-);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<TalentSonarApp />
+</React.StrictMode>
+)
